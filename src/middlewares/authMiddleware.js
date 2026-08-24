ecbb805ed02a3e7900508d7f94d017ab74f5ab81
ahad-salim <ahadsalim342gmail.com>
2026-08-24 06:25:16 +0100

    created the authMiddleware function in the middle ware folder

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,8 @@
+export default function requireAuth(req, res, next) {
+  if (!req.session.userId) {
+    return res.status(401).json({
+      erreor: "Authentication Required",
+    });
+  }
+  next();
+}
