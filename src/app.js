373166ddb34c5753509d399c8ce828b55b9026da
ahad-salim <ahadsalim342gmail.com>
2026-08-25 14:26:06 +0100

    modified the app.js file

--- src/app.js
+++ src/app.js
@@ -3,20 +3,12 @@ import errorHandler from "../src/middlewares/errorHandler.js";
 import authRoutes from '../src/routes/authRoutes.js'
 import session from "express-session"
 import "dotenv/config"
+import sessionConfig from "./config/sessions.js";
 
 const app = express();
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: false,
-        maxAge: 1000 * 60 * 60 
-    }
-}))
+app.use(express.urlencoded({extended: true}))
+app.use(sessionConfig)
 
 app.use(express.json());
 app.use('/api/v1/auth', authRoutes)
