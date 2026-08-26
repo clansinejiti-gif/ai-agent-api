24bc47d02892de3643a85655b8941a0cdc2a8715
SyntaxSultan <clansinejiti@gmail.com>
2026-08-26 12:20:47 +0100

    Modified the data types for the user models and added default values to empty string

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -1,50 +1,64 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: function (v) {
-        return v.trim().split(/\s+/).length >= 2;
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return v.trim().split(/\s+/).length >= 2;
+        },
+        message: "Please enter at least two names (e.g. John Doe)",
       },
-      message: "Please enter at least two names (e.g. John Doe)",
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: "Invalid email format",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 15,
+    },
+    role: {
+      type: String,
+      default: "student",
+      enum: ["student", "admin"],
+    },
+    academicLevel: {
+      type: String,
+      default: " ",
+    },
+    major: {
+      type: String,
+      default: " ",
+    },
+    targetRole: {
+      type: String,
+      default: " ",
+    },
+    skills: [String],
+    preferredGenres: [String],
+    learningStyle: {
+      type: String,
+      default: " ",
     },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    validate: {
-      validator: function (v) {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
-      },
-      message: "Invalid email format",
-    },
+  {
+    timestamps: true,
   },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 15,
-  },
-  role: {
-    type: String,
-    default: "student",
-    enum: ["student", "admin"],
-  },
-  academicLevel: String,
-  major: String,
-  targetRole: String,
-  skills: [String],
-  preferredGenres: [String],
-  learningStyle: String,
-},
-{
-    timestamps: true
-});
+);
 
 export default mongoose.model("User", userSchema)
