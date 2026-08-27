abe235d12471f0151d784b4b7d0b0251fad6bb61
SyntaxSultan <clansinejiti@gmail.com>
2026-08-27 13:23:23 +0100

    Update swagger ui

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5500}`,
+        url: `http://localhost:${process.env.PORT}`,
         description: "Development Server",
       },
     ],
