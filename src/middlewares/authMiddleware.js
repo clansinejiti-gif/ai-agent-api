b526e0da0c3370fe45ff07a86478b3c98dcb74e6
ahad-salim <ahadsalim342gmail.com>
2026-08-27 15:34:43 +0100

    added the json respond format

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,10 +1,11 @@
 function requireAuth(req, res, next) {
   if (!req.session.userId) {
     return res.status(401).json({
+      success: "false",
       error: "Authentication Required",
     });
   }
   next();
 }
 
-export default requireAuth
+export default requireAuth;
