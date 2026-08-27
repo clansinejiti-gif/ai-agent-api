e0e8219e13b2ecbd90c3a47b4f44dfb044e81282
SyntaxSultan <clansinejiti@gmail.com>
2026-08-27 08:27:24 +0100

    Created the track models that will store the tracks in the  database

---
+++ src/models/careerTrackModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const careerTrackSchema = new mongoose.Schema({
+  trackId: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  title: {
+    type:String,
+    required: true
+  },
+  keySkills: [String],
+  industryDemand: {
+    required: true,
+    type: String , 
+    enum: ["high", "low", "medium"],
+    default: "medium"
+  }
+}
+);
+
+export default mongoose.model("careerTrack", careerTrackSchema)
