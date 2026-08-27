8d4ad35cf87bf8ff5360ec111e70ff4bf9284e35
SyntaxSultan <clansinejiti@gmail.com>
2026-08-27 23:31:53 +0100

    Implemented production checks

--- src/config/sessions.js
+++ src/config/sessions.js
@@ -15,7 +15,7 @@ const sessionConfig = session({
         maxAge: 7*24*60*60*1000,
         httpOnly: true,
         secure: isProd, //changed to true
-        sameSite: 'strict'
+        sameSite: isProd? 'none' : 'lax'
     }
 })
 
