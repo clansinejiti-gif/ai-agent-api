cdf7400f254c3d4dc12cbba756af3d19a50b47dd
SyntaxSultan <clansinejiti@gmail.com>
2026-08-27 23:04:17 +0100

    Changed the hardcoded url for swager docs

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT}`,
+        url: process.env.SWAGGER_LINK,
         description: "Development Server",
       },
     ],
