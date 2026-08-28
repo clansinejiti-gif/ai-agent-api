fc61669539400ce491e2c9e59add3074f4e9a3ac
ahad-salim <ahadsalim342gmail.com>
2026-08-28 03:02:09 +0100

    edited the responses for register and login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,22 +1,29 @@
-import { loginUser } from "../services/authService.js"
+import { loginUser } from "../services/authService.js";
 
 const login = async (req, res, next) => {
-    try {
-        const result = await loginUser(req.body);
+  try {
+    const result = await loginUser(req.body);
 
-        if (!result.success) {
-            return res.status(401).json({ success: false, error: { code: "INVALID_CREDENTIALS", message: result.message }})
-        }
-
-        req.session.userId = result.data.id;
-        req.session.role = result.data.role;
-        req.session.email = result.data.email;
-
-        res.status(200).json({ success: true, message: "Login successful", data: result.data });
-    } catch(err) {
-        console.error("login error:", err);
-        next(err);
+    if (!result.success) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          error: { code: "INVALID_CREDENTIALS", message: result.message },
+        });
     }
-}
 
-export { login }
+    // req.session.userId = result.data.id;
+    // req.session.role = result.data.role;
+    // req.session.email = result.data.email;
+
+    res
+      .status(200)
+      .json({ success: true, message: "Login successful", data: result.data });
+  } catch (err) {
+    console.error("login error:", err);
+    next(err);
+  }
+};
+
+export { login };
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -5,10 +5,21 @@ const registerUser = async (req, res, next) => {
     const result = await registerNewUser(req.body);
 
     if (!result.success) {
-      return res.status(409).json("User not registered");
+      return res.status(409).json("User already exist");
     }
 
-    res.status(201).json("Account Created Successfully");
+    const id = req.session.userId;
+    const fullName = result.data.fullName;
+    const email = result.data.email;
+    const role = result.data.role;
+
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Account Created Successfully",
+        data: { id, fullName, email, role },
+      });
     console.log(result);
   } catch (err) {
     console.error("register error:", err);
